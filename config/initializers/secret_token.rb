ff22fb8eceae1bfdd444f4b1c29e9f6bbe0141ae
kurriking@gmail.com <kurriking@gmail.com>
2015-02-21 23:05:22 -0500

    Link Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Radit::Application.config.secret_key_base = '8ed200c67337f0b775fc1ae116474d00c896c28c05d9a6a23da1cb51434dff8c31b7d2994fbd256f747971edefce32ed58220577899a43c0fbff9b0dbc98ddd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Radit::Application.config.session_store :cookie_store, key: '_radit_session'
